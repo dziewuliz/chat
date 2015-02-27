4b5d5c59cc7cadb93964548b10ea064011238daf
Petras Kimbrys <petras.kimbrys@mail.rakuten.com>
2015-02-27 10:38:36 +0900

    fixed missing semicolons, unsued classes (ICW-169)

--- js/chat.js
+++ js/chat.js
@@ -1,35 +1,30 @@
-  $(document).ready(function () {
-      var counter = 0;
-      var $input = $("textarea#input");
+$(document).ready(function () {
+    var counter = 0;
+    var $input = $("textarea#input");
 
-      $(".colorize").click(function () {
-          $(this).prevAll("textarea").toggleClass("color");
-      });
+    $(".colorize").click(function () {
+        $(this).prevAll("textarea").toggleClass("color");
+    });
 
-      $(".remove").click(function () {
-          var $messagebox = $(this).parent(".messagebox")
-          if ($messagebox.attr("id") !== "messagebox") {
-              $messagebox.remove();
-          }
-      });
+    $(".remove").click(function () {
+        var $messagebox = $(this).parent(".messagebox");
+        if ($messagebox.attr("id") !== "messagebox") {
+            $messagebox.remove();
+        }
+    });
 
-      $("#add").click(function () {
-          var $text = $input.val();
+    $("#add").click(function () {
 
-          if ($text == "") {
-              alert("Empty? Seriously?")
-          } else {
+        counter++;
 
-              counter++;
+        var $cloned = $("#messagebox").clone(true).attr("id", "messagebox" + counter);
+        var $eachmessagebox = $("#messagebox" + counter);
 
-              var $cloned = $("#messagebox").clone(true).attr("id", "messagebox" + counter);
-              var $eachmessagebox = $("#messagebox" + counter);
+        $("#messages").prepend($cloned)
+        $("#messagebox" + counter).addClass("topmargin").children("textarea").text('');
+        $("#messagebox" + counter).prepend("<hr>");
+        $input.val("");
+        $input.removeClass("color");
 
-              $("#messages").prepend($cloned)
-              $("#messagebox" + counter).addClass("topmargin").children("textarea").text($text);
-              $("#messagebox" + counter).prepend("<hr>");
-              $input.val("");
-              $input.removeClass("color");
-          }
-      });
-  });
+    });
+});
